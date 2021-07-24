233409c84039b5a24be1c13ce64ba3dc46aa68d4
avilalopes <82402280+avilalopes@users.noreply.github.com>
2021-07-23 22:00:07 -0300

    Add files via upload

---
+++ DividindoXporY/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Desafio {
+    static void Main() {
+        Console.WriteLine("Informe a quantidade de pares de valores inteiros (X e Y):");          
+        int limit = Int32.Parse(Console.ReadLine());
+        for (int i = 0; i < limit; i++) 
+        {
+            Console.WriteLine("Informe 2 valores:");            
+            string[] line = Console.ReadLine().Split(" ");
+            double X = double.Parse(line[0]);
+            double Y = double.Parse(line[1]);
+            if (Y == 0) 
+            {
+                Console.WriteLine("divisao impossivel");
+            } 
+            else 
+            {
+                double divisao = X/Y; // Digite aqui o calculo da divisao
+                Console.WriteLine(divisao.ToString("0.0"));
+            }
+        }
+    }
+}
